8a3052a31cbd3cb50f1b0a70e3166e98506d15e3
MBXXYZ <matty2204@hotmail.com>
2020-01-12 14:16:29 +0000

    Added new command

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js');
 const { prefix , token, giphyToken } = require('./config.json');
 const client = new Discord.Client();
+const snekfetch = require('snekfetch');
 
 var GphApiClient = require('giphy-js-sdk-core')
 giphy = GphApiClient(giphyToken)
@@ -33,37 +34,66 @@ client.on('message', message => {
 			)
 			
 			
-		}}else if(message.content.startsWith(`${prefix}who`)) {
-	
-			message.channel.send("My creator is M_BX_XYZ!");
-			
-		}else if (message.content.startsWith(`${prefix}id`)) {
-			message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-		}
-		else if (message.content === 'ping') {
-			// Send "pong" to the same channel
-			message.channel.send('Pong');
-		  }
-		else if (message.content === 'hello') {
-			 message.channel.send('Hi!')
-		}
-		else if (message.content.startsWith(`${prefix}channel`)) {
-			message.channel.send('https://www.youtube.com/channel/UCoXW4KGupOexeew4H9iqXHA')
-		}
-		else if (message.content.startsWith(`${prefix}n3in`)) {
-			message.channel.send('https://www.youtube.com/channel/UCx3SjVmMirSrOddBfqSy6hg')
-		}
-		else if (message.content.startsWith(`${prefix}give`)) {
-			message.channel.send('')
-		}
-		else if (message.content.startsWith(`${prefix}welcome`)) {
-			message.channel.send('Hello' + member.displayName + '!')
+}}else if(message.content.startsWith(`${prefix}who`)) {
 
-		}
-		else if (message.content === 'help') {
-			let helpList = '```ping\nhello\nm!channel\nm!n3in\nm!welcome\nm!give```';
-			message.channel.send(helpList);
-		}
+	message.channel.send("My creator is M_BX_XYZ!");
+	
+}else if (message.content.startsWith(`${prefix}id`)) {
+	message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
+}
+else if (message.content === 'ping') {
+	// Send "pong" to the same channel
+	message.channel.send('Pong');
+	}
+else if (message.content === 'hello') {
+		message.channel.send('Hi!')
+}
+else if (message.content.startsWith(`${prefix}channel`)) {
+	message.channel.send('https://www.youtube.com/channel/UCoXW4KGupOexeew4H9iqXHA')
+}
+else if (message.content.startsWith(`${prefix}n3in`)) {
+	message.channel.send('https://www.youtube.com/channel/UCx3SjVmMirSrOddBfqSy6hg')
+}
+else if (message.content.startsWith(`${prefix}give`)) {
+	message.channel.send('')
+}
+else if (message.content.startsWith(`${prefix}welcome`)) {
+	message.channel.send('Hello' + message.author.username + '!')
+
+}
+else if (message.content === `${prefix}help`) {
+	let helpList = '```ping\nHello there!\nm!channel - Displays my makers channel\nm!n3in - Displays the N3IN Clan channel!\nm!welcome - Say hi to\nm!give```\nIf you have any questions, message @matty.p.b';
+	message.channel.send(helpList);
+}
+else if (message.content.startsWith(`${prefix}avatar`)) {
+	message.reply(message.author.avatarURL);
+}
+else if (message.content === `${prefix}server`) {
+	message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+}
+exports.run = async (client, message, args) => {
+if (message.content === `${prefix}meme`) {
+
+ 
+	const { body } = await snekfetch
+		.get('https://www.reddit.com/r/dankmemes/top/?t=week')
+		.query({ limit: 800 });
+	const allowed = message.channel.nsfw ? body.data.children : body.data.children.filter(post => !post.data.over_18);
+	if (!allowed.length) return message.channel.send('It seems we are out of memes');
+	const randomnumber = Math.floor(Math.random() * allowed.length)
+	const embed = new Discord.RichEmbed()
+	.setColor(0x00A2E8)
+	.setTitle(allowed[randomnumber].data.title)
+	.setDescription("Posted by: " + allowed[randomnumber].data.author)
+	.setImage(allowed[randomnumber].data.url)
+	.addField("Other info:", "Up votes: " + allowed[randomnumber].data.ups + " / Comments: " + allowed[randomnumber].data.num_comments)
+	.setFooter("r/dankmemes")
+	message.channel.send(embed)
+} 
+
+}
+
+	
 	
 	}
 )
